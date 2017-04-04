36cb63bd7496bc6f46cb403abd3fd4e7ba2661bd
yuHLe <yuHLe88@gmail.com>
2017-04-04 02:40:40 -0400

    Testing rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
+import * as Rx from 'rxjs/Rx';
+
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>GitHub Profile</h1>
+
+  <input id="search">
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  name = 'Angular';
+  constructor() {
+    const search$ = Rx.Observable
+      .of('Hello World!')
+      .subscribe(console.log);
+  }
+}
